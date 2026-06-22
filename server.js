bd242a02aa188f13d8eccf42c29a0afd3cc38e4b
orangerealesstate <orangerealesstate@gmail.com>
2026-06-22 23:24:36 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+app.use(express.static(__dirname));
+
 app.get("/", (req, res) => {
-  res.send("TBILISI ARENDA API is running");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
