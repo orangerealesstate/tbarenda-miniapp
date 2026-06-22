3770eae83939a36de49513ba0238af1f96f8f2a0
orangerealesstate <orangerealesstate@gmail.com>
2026-06-23 00:02:18 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
 
 const app = express();
 
@@ -8,6 +9,15 @@ app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/posts", (req, res) => {
+  try {
+    const data = fs.readFileSync("posts.json");
+    res.json(JSON.parse(data));
+  } catch {
+    res.json({ text: "No posts yet" });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
@@ -24,13 +34,14 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, {
 bot.on("channel_post", (msg) => {
   console.log("NEW POST:");
 
-  if (msg.caption) {
-    console.log(msg.caption);
-  } else if (msg.text) {
-    console.log(msg.text);
-  } else {
-    console.log("Photo without caption");
-  }
+  const text = msg.caption  msg.text  "Photo without caption";
+
+  console.log(text);
+
+  fs.writeFileSync(
+    "posts.json",
+    JSON.stringify({ text })
+  );
 });
 
 console.log("Bot started");
