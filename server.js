19beef2061b182fa4cb3841c3aa7c5f295f2ad20
orangerealesstate <orangerealesstate@gmail.com>
2026-06-22 23:54:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const TelegramBot = require("node-telegram-bot-api");
 
 const app = express();
 
@@ -14,3 +15,22 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Telegram Bot
+const bot = new TelegramBot(process.env.BOT_TOKEN, {
+  polling: true
+});
+
+bot.on("channel_post", (msg) => {
+  console.log("NEW POST:");
+
+  if (msg.caption) {
+    console.log(msg.caption);
+  } else if (msg.text) {
+    console.log(msg.text);
+  } else {
+    console.log("Photo without caption");
+  }
+});
+
+console.log("Bot started");
