8faba9c508e3cee8bf5b00599d967bb79beebdab
orangerealesstate <orangerealesstate@gmail.com>
2026-06-26 10:32:00 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,23 @@
 const TelegramBot = require("node-telegram-bot-api");
 const fs = require("fs");
 
-console.log(process.env.BOT_TOKEN);
-
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
-  polling: true
+  polling: true,
 });
+
 bot.on("channel_post", (msg) => {
-const text = msg.caption || msg.text || "";
+  const text = msg.caption  msg.text  "";
 
   let posts = [];
 
   try {
     posts = JSON.parse(fs.readFileSync("data.json", "utf8"));
-  } catch {
+  } catch (e) {
     posts = [];
   }
-let image = "https://picsum.photos/600/400";
+
+  const image = "https://picsum.photos/600/400";
+
   posts.unshift({
     title: text.split("\n")[0],
     price: "",
